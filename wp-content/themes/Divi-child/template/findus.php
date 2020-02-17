0ff5f0ce4ed30e4325d9abae129c642abb6fbf95
hudamerchant <hudazehra2510@gmail.com>
2020-02-17 13:07:38 +0500

    stripe

---
+++ wp-content/themes/Divi-child/template/findus.php
@@ -0,0 +1,8 @@
+<?php
+/*Template Name:custom Findus;*/
+get_header();
+?>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3619.5467358777946!2d67.05700631501661!3d24.87932585062847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33eed775959a5%3A0x653a6ae6b3f6a391!2sGenetech%20Solutions!5e0!3m2!1sen!2s!4v1581926256012!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+<?php
+get_footer();
+?>
